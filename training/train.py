ecbc3e6937ad5f6e922ab27a8fba66cb32d6d06d
tyqqj0 <tyqqj0@163.com>
2023-03-30 22:22:11 +0800

    彻底修复pre_features问题

--- training/train.py
+++ training/train.py
@@ -17,6 +17,31 @@ from utilis.meters import AverageMeter, ProgressMeter
 from training.reweighting import weight_learner
 
 
+# 检查每个类别和和哪些特征相关
+
+def compute_correlation(output, cfeatures):
+    # 计算 output 和 cfeatures 的均值
+    output_mean = output.mean(dim=0, keepdim=True)
+    cfeatures_mean = cfeatures.mean(dim=0, keepdim=True)
+
+    # 分别计算 output 和 cfeatures 的零均值数据
+    output_zero_mean = output - output_mean
+    cfeatures_zero_mean = cfeatures - cfeatures_mean
+
+    # 计算 output 和 cfeatures 的协方差
+    # 计算 output 和 cfeatures 的协方差
+    cov = output_zero_mean.t().matmul(cfeatures_zero_mean) / (batch_size - 1)
+
+    # 计算 output 和 cfeatures 的标准差
+    output_std = output_zero_mean.std(dim=0, unbiased=False)
+    cfeatures_std = cfeatures_zero_mean.std(dim=0, unbiased=False)
+
+    # 计算相关系数矩阵
+    correlation_matrix = cov / (output_std[:, None] * cfeatures_std[None, :])
+
+    return correlation_matrix
+
+
 def train(train_loader, model, criterion, optimizer, epoch, args, tensor_writer=None):
     ''' TODO write a dict to save previous featrues  check vqvae,
         the size of each feature is 512, os we need a tensor of 1024 * 512
@@ -49,7 +74,20 @@ def train(train_loader, model, criterion, optimizer, epoch, args, tensor_writer=
         target = target.cuda(args.gpu, non_blocking=True)
 
         output, cfeatures = model(images)
+        ##################################################
+        # 计算相关系数矩阵
+        correlation_matrix = compute_correlation(output, cfeatures)
 
+        # 找到与每个 output 最相关的 cfeatures
+        # most_related_cfeatures_indices = correlation_matrix.argmax(dim=1).tolist()
+
+        top_k = 5
+        most_related_cfeatures_indices = correlation_matrix.topk(top_k, dim=1).indices.tolist()
+
+        # 输出结果
+        for output_idx, cfeature_idx in enumerate(most_related_cfeatures_indices):
+            print(f"Output {output_idx}({output[output_idx]}) is most related to cfeature {cfeature_idx}")
+        ##################################################
         if args.distributed is False and args.gpu is None:
             # 当使用多GPU训练时，模型实际保存位置在model.module中
             # 因此需要从model.module中获取参数
@@ -57,8 +95,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args, tensor_writer=
         else:
             pre_features, pre_weight1 = model.get_prefeatures()
         # 输出pre_features, pre_weight1
-        print('pre_features', pre_features[0:10])
-        print('pre_weight1', pre_weight1[0:10])
+        # print('pre_features', pre_features[0:10])
+        # print('pre_weight1', pre_weight1[0:10])
         if epoch >= args.epochp:
             weight1, pre_features, pre_weight1 = weight_learner(cfeatures, pre_features, pre_weight1, args, epoch, i)
 
